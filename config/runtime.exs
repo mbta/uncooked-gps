d2e38ae3aeebe401fd68d805fcf9f64bdddabb74
Preston Mueller <pmueller@mbta.com>
2025-05-22 17:21:37 -0400

    Upload to S3

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,6 @@
 import Config
 
 config :uncooked_gps, ocs_url: System.fetch_env!("OCS_URL")
-config :uncooked_gps, s3_path: System.fetch_env!("S3_BUCKET")
 config :uncooked_gps, team_email: System.fetch_env!("TEAM_EMAIL")
+config :uncooked_gps, s3_bucket: System.get_env("S3_BUCKET")
+config :uncooked_gps, s3_path: System.get_env("S3_PATH")
--- lib/uncooked_gps.ex
+++ lib/uncooked_gps.ex
@@ -1,7 +1,11 @@
 defmodule UncookedGps do
+  alias ExAws.S3
+
   def main(_args) do
     url = Application.fetch_env!(:uncooked_gps, :ocs_url)
     team_email = Application.fetch_env!(:uncooked_gps, :team_email)
+    s3_bucket = Application.get_env(:uncooked_gps, :s3_bucket)
+    s3_path = Application.get_env(:uncooked_gps, :s3_path)
 
     user_agent = "UncookedGps/0.1.0 (#{team_email})"
     IO.puts("[#{user_agent}] Fetching #{url}")
@@ -36,6 +40,15 @@ defmodule UncookedGps do
       end)
       |> Map.delete(nil)
 
-    File.write!("LightRailRawGPS.json", JSON.encode_to_iodata!(vehicles))
+    data = JSON.encode_to_iodata!(vehicles)
+
+    if s3_bucket != nil do
+      IO.puts("Writing to s3://#{s3_bucket}/#{s3_path}...")
+      S3.put_object(s3_bucket, s3_path, data)
+    else
+      path = "LightRailRawGPS.json"
+      IO.puts("Writing to file://#{path}...")
+      File.write!(path, data)
+    end
   end
 end
