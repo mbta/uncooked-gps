d22ab3bed22f85dd21ee107ad64938e3bb759c5d
Preston Mueller <pmueller@mbta.com>
2025-06-04 15:26:00 -0400

    fixup: health.ex

--- lib/health.ex
+++ lib/health.ex
@@ -3,12 +3,14 @@ defmodule UncookedGps.Health do
 
   require Logger
 
-  @spec healthy?() :: any()
+  @spec healthy?() :: :ok
   def healthy? do
     ocs_url = Application.fetch_env!(:uncooked_gps, :ocs_url)
     uri = URI.new!(ocs_url)
 
+    # This raises on failure
     Http.head("#{uri.scheme}://#{uri.host}:#{uri.port}")
     Logger.info("healthy")
+    :ok
   end
 end
