dc58904fbc3090f08dfbda02997a9df5d62a7517
Preston Mueller <pmueller@mbta.com>
2025-06-06 11:53:09 -0400

    fix: print only warnings and errors during test

--- config/test.exs
+++ config/test.exs
@@ -2,3 +2,7 @@ import Config
 
 config :uncooked_gps, ocs_url: "http://localhost/test"
 config :uncooked_gps, team_email: "test@localhost"
+
+config :logger,
+  # Print only warnings and errors during test
+  level: :warning
