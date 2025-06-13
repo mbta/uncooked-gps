cd8d4ba75c694ebdff531830706d42ed843599b6
Preston Mueller <pmueller@mbta.com>
2025-06-13 11:05:00 -0400

    fixup: add to test.exs

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,7 @@ import Config
 
 config :uncooked_gps, ocs_url: "http://localhost/test"
 config :uncooked_gps, team_email: "test@localhost"
+config :uncooked_gps, poll_delay_ms: "86400000"
 
 config :logger,
   # Print only warnings and errors during test
