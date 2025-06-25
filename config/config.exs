2e2f8e8ff4e4e3df03bebb8aa089a03f7a4569b9
Preston Mueller <pmueller@mbta.com>
2025-06-25 10:46:06 -0400

    feat: add FETCH_ENABLED (to disable fetch in dev) (#8)

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,6 @@ import Config
 config :elixir, :time_zone_database, Tz.TimeZoneDatabase
 config :ex_aws, http_client: ExAws.Request.Req
 
+config :uncooked_gps, fetch_enabled: false
+
 import_config "#{config_env()}.exs"
